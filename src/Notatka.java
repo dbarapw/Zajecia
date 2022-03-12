feb98d1f9b6964538ad8602cb50d5cc3b94b5629
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-12 13:29:21 +0100

    WERSJA 3

--- src/Notatka.java
+++ src/Notatka.java
@@ -1,42 +1,3 @@
-import java.util.Scanner;
-
-public class Notatka{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String dzialanie = "1";
-        while (dzialanie != "x") {
-            System.out.println("Podaj dzialanie(d-dodawanie, o-odejmowanie, x-wyjscie): ");
-            String a = String.valueOf(input.nextInt());
-        }
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*import java.util.Scanner;
@@ -65,3 +26,72 @@ public class Testowa {
 
     */
 
+/////TYPY LOSOWANIA LICZB/////
+//double random = Math.random() * 100;
+
+//int random = (int)(Math.random() * 101);
+
+//Random rand = new Random();
+//int n = rand.nextInt(50);
+
+
+/////Array i For/////
+//for(int i = 0; i<10; i=i+3){
+//    System.out.println(i*2);
+//}
+//int a[]={123,123,32,10,20};
+//    for(int i = 0;i<=5;i++) {
+//    System.out.print(i);
+//    System.out.print("       ");
+//    System.out.print(a[i]);
+//    System.out.print("       ");
+//    System.out.print(a[i]*100);
+//    System.out.println("");
+//}
+
+//public static void main(String[] args) {
+//    int a[]={123,123,32,10,20};
+//    a[0]+=2000;
+//    for(int i =0; i<a.length;i++){
+//        System.out.println(a[i]);
+
+//import java.util.Scanner;
+//
+//public class Kalkulator {
+//    public static void main(String[] args) {
+//        //Tworzymy obiekt Scanner
+//        Scanner input = new Scanner(System.in);
+//        //Deklarujemy zmienną string aby jej później używać
+//        String dzialanie = "1";
+//        //warunek pętli while jeśli jest true to ta pętla zawsze się wykonuje chyba że natrafi na break;
+//        while (true){
+//            //Pytamy o dzialanie
+//            System.out.println("Wyjscie(x),Dodawanie(d),Odejmowanie(o),Mnozenie(m),Dzielenie(dz)");
+//            dzialanie = input.next();
+//            //Przerwanie aplikacji jesli x
+//            if(dzialanie.equals("x")){
+//                break;
+//            }
+//            //Pytamy o liczby
+//            System.out.println("Podaj pierwszą:");
+//            int a = input.nextInt();
+//            System.out.println("Podaj drugą:");
+//            int b = input.nextInt();
+//            //Wykonywanie działań za pomocą switcha
+//            switch(dzialanie){
+//                case "d":
+//                    System.out.println(String.valueOf(a)+"+"+String.valueOf(b)+"="+(a+b));
+//                    break;
+//                case "o":
+//                    System.out.println(String.valueOf(a)+"-"+String.valueOf(b)+"="+(a-b));
+//                    break;
+//                case "m":
+//                    System.out.println(String.valueOf(a)+"*"+String.valueOf(b)+"="+(a*b));
+//                    break;
+//                case "dz":
+//                    System.out.println(String.valueOf(a)+"/"+String.valueOf(b)+"="+(a/b));
+//                    break;
+//                default:
+//                    break;
+//            }
+//
---
+++ src/Random.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class RandandFor {
+    public static void main(String[] args) {
+    }
+    /////TYPY LOSOWANIA LICZB/////
+    //double random = Math.random() * 100;
+
+    //int random = (int)(Math.random() * 101);
+
+    //Random rand = new Random();
+    //int n = rand.nextInt(50);
+
+
+    /////Array i For/////
+    //for(int i = 0; i<10; i=i+3){
+    //    System.out.println(i*2);
+    //}
+    //int a[]={123,123,32,10,20};
+    //    for(int i = 0;i<=5;i++) {
+    //    System.out.print(i);
+    //    System.out.print("       ");
+    //    System.out.print(a[i]);
+    //    System.out.print("       ");
+    //    System.out.print(a[i]*100);
+    //    System.out.println("");
+    //}
+
+    //public static void main(String[] args) {
+    //    int a[]={123,123,32,10,20};
+    //    a[0]+=2000;
+    //    for(int i =0; i<a.length;i++){
+    //        System.out.println(a[i]);
+    //    }
+    //}
+
+    //////Tworzenie tablicy od 1 do 10000
+    //int b[] = new int[10000];
+    //for(int i = 0;i<b.length;i++){
+    //    b[i]=i+1;
+    //}
+
+    ///////Dwuwymiarowe tablice
+    //int a[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    //    for(int i = 0; i<a.length;i++) {
+    //    for (int j = 0;j<a[i].length;j++){
+    //        System.out.println(a[i][j]);
+    //    }
+    //}
+}
