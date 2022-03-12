a228a6c1f5901740624a3de8b68b2cc9a364dead
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-12 13:51:57 +0100

    WERSJA 3

---
+++ src/Auto.java
@@ -0,0 +1,12 @@
+public class Auto {
+    public String kolor;
+    public int predkosc;
+
+    Auto(){
+        kolor =
+    }
+    public void jedzie() {
+        System.out.println("Auto jedzie");
+        System.out.println("Auto stoi");
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,14 @@
+public class Program {
+    public static void main(String[] args) {
+        Auto czerwone = new Auto();
+        Auto niebieskie = new Auto();
+
+
+        System.out.println(czerwone.kolor);
+        System.out.println(niebieskie.kolor);
+
+        czerwone.jedzie();
+
+
+    }
+}
