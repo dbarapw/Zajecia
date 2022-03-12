424dea6d8d3d69b7a4343223298f7b335305fe6c
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-12 10:24:32 +0100

    WERSJA 2

--- src/Testowa.java
+++ src/Testowa.java
@@ -1,5 +1,67 @@
+import java.util.Scanner;
+
 public class Testowa {
     public static void main(String[] args) {
-        System.out.println("Zajecia");
+        Scanner input = new Scanner(System.in);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import java.util.Scanner;
+public class Testowa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String dzialanie = "1";
+        while ( dzialanie!= "x") {
+            System.out.println("Podaj dzialanie(d-dodawanie, o-odejmowanie, x-wyjscie): ");
+            String a = String.valueOf(input.nextInt());
+
     }
 }
+    /*
+    void - nic
+    short / int / long - liczba calkowita
+    char - jeden znak
+    float - liczba zmienno przecinkowa
+    boolean - True False
+    String - ciag znakow
+    TYP NAZWA = WARTOSC;
+    TYP NAZWA2 = WARTOSC;
+    int a = 100
+    int b = 10;
+    float a = 10.21f;
+
+    */
+
