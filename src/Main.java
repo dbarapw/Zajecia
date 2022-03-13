8caa5e5966d5ad90ca3d190f264850b0621c6670
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-13 09:11:26 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         int tab1[] = {12, 21, 33, 41, 56, 67, 57};
         int tab2[] = {52, 33, 14, 55, 67, 78, 94};
         //FOR EACH
-        for (int liczba : tab1);
-        for (int liczba : tab2){
+        for (int liczba : tab1) ;
+        for (int liczba : tab2) {
             liczba += 1;
             // liczba+=1 oznacza, ze liczba bedzie wieksza o 1
         }
@@ -15,11 +15,20 @@ public class Main {
             // \n
             System.out.print(tab2[i]);
             System.out.print("\t");
-            System.out.println(tab1[i]*tab2[i]);
-
+            System.out.println(tab1[i] * tab2[i]);
         }
-
-
+        // double a = 3+Math.PI;
+        // liczba 3 + liczba PI
+        for (int i = 0; i < tab1.length; i++) {
+            int temp = Math.max(tab1[i], tab2[i]);
+            if (temp == tab1[i]) {
+                System.out.println("Tab1 nie jest wieksza");
+            } else {
+                System.out.println("Tab1 jest wieksza");
             }
+
+
         }
+    }
+}
 
