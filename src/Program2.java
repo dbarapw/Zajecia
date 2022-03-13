89adc619bb834e27c0c5db098e0ffc5cff2418e0
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-13 12:05:08 +0100

    TEST

---
+++ src/Program2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Program2 {
+    public static void main(String[] args) {
+        //tworzenie listy obiektów klasy Auto
+        ArrayList<Auto> lista_aut = new ArrayList<>();
+        for(int i = 0; i<3;i++){
+            lista_aut.add(new Auto());
+        }
+        for(Auto i : lista_aut){
+            System.out.println(i.toString());
+        }
+
+    }
+}
