fa4ec789739d72007370d8e9fd086aafadac5f7d
STANOWISKO_D001 <dbara.pw@gmail.com>
2022-03-13 11:37:49 +0100

    TEST

--- src/Plik.java
+++ src/Plik.java
@@ -1,16 +1,29 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+
 // Tworzenie pliku
 public class Plik {
     public static void main(String[] args) throws IOException {
         try {
             File plik = new File("plik_tekstowy.txt");
-            if(plik.createNewFile()){
+            if (plik.createNewFile()) {
                 System.out.println("Utworzono plik");
-            }else{
+            } else {
                 System.out.println("Nie udalo sie utworzyc pliku");
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // ZAPIS DO PLIKU
+        try {
+            FileWriter plik = new FileWriter("plik_tekstowy.txt");
+            plik.write("zapiszmy cos na pliku");
+            plik.write("\nzapiszmy cos na pliku");
+            plik.close();
+
+        } catch (IOException e) {
+            System.out.println("Nie udalo sie zapisac do pliku");
             e.printStackTrace();
         }
     }
